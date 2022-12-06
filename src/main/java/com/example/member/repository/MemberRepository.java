2283cd4c7607cc8a2cad2ac9928257518c9fc7c9
jaesiki96 <jaesiki96@naver.com>
2022-12-06 16:32:04 +0900

    21_수정(axios) 기능 수정

--- src/main/java/com/example/member/repository/MemberRepository.java
+++ src/main/java/com/example/member/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package com.example.member.repository;
 import com.example.member.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.reflect.Member;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
